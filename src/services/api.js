cbc8a80648fa88267296fac91cb00f61b7d07432
Umchristelle <umutonichristella17@gmail.com>
2026-03-29 15:42:58 +0200

    Add .env with API base URL for Vercel deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://student-management-system-backend.up.railway.app/api/vehicle-service",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
 });
 
